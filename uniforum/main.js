b3e3a06a5a828eef388d5e214f5cf6571c22da73
byakuyaawastaken <68425437+byakuyaawastaken@users.noreply.github.com>
2020-09-17 00:00:09 +0200

    Add files via upload

---
+++ uniforum/main.js
@@ -0,0 +1,12 @@
+var login = prompt("intrabit login: ");
+
+var password = prompt("ignoro ingressus: ");
+
+if (password=="1234", login=="testlog") 
+{
+	window.location="true1.html";
+}
+else 
+{
+	document.write("debemus flectere et non opus est tibi hic");
+}
---
+++ uniforum/true1.js
@@ -0,0 +1,6 @@
+document.write("<div>Witam!</div><br>");
+
+var update_text = "[UPDATE LOG]: ";
+
+document.write(update_text,"Spróbuję ogarnąć jakiś dobry hosting<br>");
+document.write(update_text,"Tylko admin tu przebywa(narazie)");
